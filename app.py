f46e18399f6785da6e458938bdf1aa6b380e47c8
testtarou-555 <testtarou378@gmail.com>
2025-01-10 13:44:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,23 @@ import pytz
 app = Flask(__name__)
 
 # 環境変数からLINE Notifyのアクセストークンとタイムゾーンを取得
-LINE_NOTIFY_ACCESS_TOKEN = os.getenv("TVb5EFJlsvu12xS78vkLwYbqCPjYrCYhtLKeQGwfAWt")
+LINE_NOTIFY_ACCESS_TOKEN = 'TVb5EFJlsvu12xS78vkLwYbqCPjYrCYhtLKeQGwfAWt'
 TIMEZONE = os.getenv('TIMEZONE', 'Asia/Tokyo')  # デフォルトは東京時間
 
 def send_line_notify():
+    message = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - 3分ごとの通知です。"
+    print(message)  # コンソールにプリント
     url = "https://notify-api.line.me/api/notify"
     headers = {
         "Authorization": f"Bearer {LINE_NOTIFY_ACCESS_TOKEN}"
     }
     data = {
-        "message": f"{datetime.now().strftime('%H:%M:%S')} - これは5分ごとの通知です！"
+        "message": message
     }
     try:
         response = requests.post(url, headers=headers, data=data)
         response.raise_for_status()
-        print(f"{datetime.now()} - メッセージを送信しました。")
+        print("LINE Notify にメッセージを送信しました。")
     except requests.exceptions.HTTPError as err:
         print(f"HTTPエラー: {err}")
     except Exception as err:
@@ -30,16 +32,4 @@ def send_line_notify():
 
 @app.route('/')
 def home():
-    return "このページはUptimeRobotによって定期的にアクセスされ、RenderのWebサービスをスリープさせないために存在します。"
-
-def schedule_job():
-    scheduler = BackgroundScheduler(timezone=TIMEZONE)
-    # 通知を5分ごとに設定
-    scheduler.add_job(send_line_notify, 'interval', minutes=5)
-    scheduler.start()
-    print("スケジューラーが開始されました。")
-
-if __name__ == "__main__":
-    schedule_job()
-    # Flaskアプリを起動
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+    return "このページはUptimeRobotによって定期的にアクセスされ、RenderのWebサービスをスリープさせないため
